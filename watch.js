e726967259c0ad53cef51396641e497aea0abe6b
Mike Orr <sluggoster@gmail.com>
2017-01-07 13:30:53 -0800

    'update' function.

--- watch.js
+++ watch.js
@@ -5,21 +5,28 @@
 var stopwatch_start = null;
 var timer = null;
 
+var update = function (id, value, value1) {
+    console.log(value === value1)
+    if (value !== value1) {
+        document.getElementById(id).innerHTML = value;
+    }
+}
+
 var tick = function() {
     var now = moment();
     var hours_minutes = now.format("h:mm")
     var seconds = now.format("ss")
     var ampm = now.format("a")
     var date = now.format("dddd, MMMM D, YYYY")
-    document.getElementById("hours-minutes").innerHTML = hours_minutes
-    document.getElementById("seconds").innerHTML = seconds
-    document.getElementById("ampm").innerHTML = ampm
-    document.getElementById("date").innerHTML = date
-    }
+    update("hours-minutes", hours_minutes, null);
+    update("seconds", seconds, null);
+    update("ampm", ampm, null);
+    update("date", date, null);
+}
 
 var init = function() {
     tick();
     timer = window.setInterval(tick, 500);
-    }
+}
 
 window.addEventListener("load", init);
