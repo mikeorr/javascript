7642bc99f178f2df7aab128d5610686ed4dd1f2c
Mike Orr <sluggoster@gmail.com>
2017-01-09 17:22:47 -0800

    Military time display and unchanged state.

--- watch.js
+++ watch.js
@@ -8,9 +8,12 @@ class Clock {
         this.fmt_seconds = null;
         this.fmt_ampm = null;
         this.fmt_date = null;
+        this.last_monent = null;
+        this.military = false;
         this.unix = null;
     }
 }
+
 
 class Stopwatch {
     constructor () {
@@ -20,6 +23,7 @@ class Stopwatch {
         this.fmt_time = null;
     }
 }
+
 
 const clock = new Clock();
 const stopwatch = new Stopwatch();
@@ -33,25 +37,42 @@ function update (id, value, value1) {
     }
 }
 
-function tick () {
-    const now = moment();
-    const fmt_hours_minutes = now.format("h:mm");
-    const fmt_seconds = now.format("ss");
-    const fmt_ampm = now.format("a");
-    const fmt_date = now.format("dddd, MMMM D, YYYY");
-    if (fmt_hours_minutes != clock.fmt_hours_minutes) {
+function tick (refresh_all=false) {
+    let now, changed, fmt_hours_minutes, fmt_seconds, fmt_ampm, fmt_date;
+    now = moment();
+    changed = (
+        refresh_all ||
+        clock.last_moment == null ||
+        !now.isSame(clock.last_moment, "second")
+        );
+    // console.log("Clock changed = %s", changed);
+    if (!changed) {
+        return;
+    }
+    // console.log("Updating clock display.");
+    clock.last_moment = now;
+    if (clock.military) {
+        fmt_hours_minutes = now.format("H:mm");
+        fmt_ampm = "";
+    } else {
+        fmt_hours_minutes = now.format("h:mm");
+        fmt_ampm = now.format("a");
+    }
+    fmt_seconds = now.format("ss");
+    fmt_date = now.format("dddd, MMMM D, YYYY");
+    if (refresh_all || fmt_hours_minutes != clock.fmt_hours_minutes) {
         get_element("hours-minutes").innerHTML = fmt_hours_minutes;
         clock.fmt_hours_minutes = fmt_hours_minutes;
     }
-    if (fmt_seconds != clock.fmt_seconds) {
+    if (refresh_all || fmt_seconds != clock.fmt_seconds) {
         get_element("seconds").innerHTML = fmt_seconds;
         clock.fmt_seconds = fmt_seconds;
     }
-    if (fmt_ampm != clock.fmt_ampm) {
+    if (refresh_all || fmt_ampm != clock.fmt_ampm) {
         get_element("ampm").innerHTML = fmt_ampm;
         clock.fmt_ampm = fmt_ampm;
     }
-    if (fmt_date != clock.fmt_date) {
+    if (refresh_all || fmt_date != clock.fmt_date) {
         get_element("date").innerHTML = fmt_date;
         clock.fmt_date = fmt_date;
     }
@@ -60,7 +81,7 @@ function tick () {
 }
 
 function init() {
-    tick();
+    tick(true);
     timer = window.setInterval(tick, 500);
 }
 
